eae9a9e6f061b8527fb06fb2d24dab9d6eebcae1
kuscah <dems14@gmail.com>
2017-05-04 16:32:15 -0400

    cambios a login

--- client/src/jsCorrector.js
+++ client/src/jsCorrector.js
@@ -1,3 +1,30 @@
-const newUser = function () {
-	this.name = "", this.lastname = "", this.email = "", this.pasword = "", this.jobtitle = "", this.fotolink = "", this.foto = null, this.ischangepassword = "T", this.idrol = 0
-}
+export default{
+
+        data () {
+            return {
+                people: {
+                    email: '',
+                    pasword: ''
+                },
+                apiUrl: 'http://localhost:8080/WebSkillsGW7/webresources'
+            }
+        },
+        mounted: function () {
+            this.$refs.videoRef.src = "static/video/VORTEX.mp4";
+            this.$refs.videoRef.play();
+        },
+        name: 'scopes-example',
+        methods: {
+            login() {
+                var data = JSON.stringify(this.people);
+                //data = JSON.parse(data);
+                this.$validator.validateAll().then(() => {
+                    // eslint-disable-next-line
+                    console.log(data);
+                    this.$http.post(this.apiUrl + '/entis.people/login', data).then((response) => {
+                        console.log(response);
+                    });
+                });
+            }
+        }
+    }
