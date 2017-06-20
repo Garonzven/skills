5041c2d349c892e915af7c0e3c2ab08bd3fa0cdf
chuckbot <cgmilan@gmail.com>
2017-06-19 21:31:51 -0400

    Add createUser Functionality

--- client/src/components/createUser.vue
+++ client/src/components/createUser.vue
@@ -11,7 +11,7 @@
                     <table class="table table-user-information table form-horizontal" style="margin-top:130px">
                       <tbody>
                         <tr>
-                          <td class="form-group"><input name="name" v-model='newUser.name' type="text" placeholder="NAME LASTNAME" class="form-control"></td>
+                          <td class="form-group"><input name="name" v-model="newUser.name" type="text" placeholder="NAME LASTNAME" class="form-control"></td>
                         </tr>
                         <tr>
                           <td class="form-group"><input name="job_title" v-model="newUser.jobtitle" type="text" placeholder="JOB TITLE" class="form-control">
@@ -42,12 +42,12 @@
                           <td class="form-group"><input name="rol" v-model="newUser.indate" type="text" placeholder="SINCE DATE" class="form-control">
                           </td>
                         </tr>
-                        <!--tr>
+                        <tr>
                           <td class="form-group">
                             <p class="control has-icon has-icon-right">
                               <input name="file" id="file" @change="onFileChange" v-validate.reject="'mimes:image/*'" :class="{ 'is-danger': errors.has('file') }" type="file" style="display:none"> <i v-show="errors.has('file')" class="fa fa-warning"></i> <span v-show="errors.has('file')" class="help-block alert alert-danger">{{ errors.first('file') }}</span></p>
                           </td>
-                        </tr-->
+                        </tr>
                       </tbody>
                     </table>
                   </div>
@@ -61,127 +61,115 @@
 </template>
 
 <script>
-    const newUser = function() {
-        this.name = '', this.lastname = '', this.email = '', this.phone = "",
-            this.location = "", this.jobtitle = "", this.fotolink = "", this.foto = "", this.idrol = {
-                idrol: "",
-                name: ""
-            }, this.skillandlevel = "", this.indate = "", this.pasword, this.skills = []
-    }
+  const newUser = function() {
+    this.name = '', this.lastname = '', this.email = '', this.phone = "",
+      this.location = "", this.jobtitle = "", this.fotolink = "", this.foto = "", this.idrol = {
+        idrol: "",
+        name: ""
+      }, this.skillandlevel = "", this.indate = "", this.pasword = []
+  }
 
-    const skillList = [];
+  const skillList = [];
 
-    export default {
-        data() {
-            return {
-                selected: [],
-                newUser: new newUser(),
-                skillList: [],
-            }
-        },
-        methods: {
-            post() {
-                let data = this.newUser;
-                data.pasword = Math.random().toString(36).substr(2, 6)
-                if (data.idrol.name == 'Super User') {
-                    data.idrol.idrol = 1
-                } else if (data.idrol.name == 'Admin') {
-                    data.idrol.idrol = 2
-                } else if (data.idrol.name == 'Watcher') {
-                    data.idrol.idrol = 3
-                } else if (data.idrol.name == 'User') {
-                    data.idrol.idrol = 4
-                }
-                console.log(data);
-                console.log(JSON.stringify(data));
-
-                $.ajax({
-                    type: "POST",
-                    url: "https://13.92.199.15:45734/WebSkillsGW7/webresources/entis.people/",
-                    data: JSON.stringify(data),
-                    contentType: "application/json",
-                    dataType: 'json',
-                    success: function() {
-                        console.log("new skill added");
-                    }
-                })
-                /*var users = []
-
-                $.ajax({
-                  url: 'http://localhost:8080/WebSkillsGW7/webresources/entis.skill/',
-                  type: 'GET',
-                  success: function (data) {
-                    users = data
-                    console.log(users)
-                  },
-                  dataType: 'json'
-                })*/
-            },
-            onFileChange(e) {
-                var files = e.target.files || e.dataTransfer.files
-                if (!files.length) return
-                this.createImage(files[0])
-            },
-            createImage(file) {
-                var image = new Image()
-                var reader = new FileReader()
-                var vm = this
-                reader.onload = (e) => {
-                    vm.newUser.fotolink = e.target.result
-                    var photo = document.getElementById("User_Pic").src = vm.newUser.fotolink
-                }
-                reader.readAsDataURL(file)
-            }
+  export default {
+    data() {
+      return {
+        selected: [],
+        newUser: new newUser(),
+        skillList: [],
+        apiUrl: 'https://13.92.199.15:45734/WebSkillsGW7/webresources'
+      }
+    },
+    methods: {
+      post() {
+        let data = this.newUser;
+        data.pasword = Math.random().toString(36).substr(2, 6)
+        if (data.idrol.name == 'Super User') {
+          data.idrol.idrol = 1
+        } else if (data.idrol.name == 'Admin') {
+          data.idrol.idrol = 2
+        } else if (data.idrol.name == 'Watcher') {
+          data.idrol.idrol = 3
+        } else if (data.idrol.name == 'User') {
+          data.idrol.idrol = 4
         }
+        console.log(data);
+        console.log(JSON.stringify(data));
+
+
+        this.$http.post(this.apiUrl + '/entis.people/', data)
+          .then(response => {
+            console.log(response)
+            //this.$auth.setToken(response.body.access_token + Date.now())
+
+            this.$router.push('/listUser')
+        })
+      },
+      onFileChange(e) {
+        var files = e.target.files || e.dataTransfer.files
+        if (!files.length) return
+        this.createImage(files[0])
+      },
+      createImage(file) {
+        var image = new Image()
+        var reader = new FileReader()
+        var vm = this
+        reader.onload = (e) => {
+          vm.newUser.fotolink = e.target.result
+          var photo = document.getElementById("User_Pic").src = vm.newUser.fotolink
+        }
+        reader.readAsDataURL(file)
+      }
     }
+  }
 
 </script>
 
 <style>
-    .submit_buttom {
-        width: 100%;
-        background-color: #f16521;
-        text-transform: uppercase;
-        border-color: #f16521;
-    }
+  .submit_buttom {
+    width: 100%;
+    background-color: #f16521;
+    text-transform: uppercase;
+    border-color: #f16521;
+  }
 
-    .submit_buttom:hover {
-        background-color: #f07020;
-        border-color: #f07020;
-    }
+  .submit_buttom:hover {
+    background-color: #f07020;
+    border-color: #f07020;
+  }
 
-    .user-row {
-        margin-bottom: 14px;
-    }
+  .user-row {
+    margin-bottom: 14px;
+  }
 
-    .user-row:last-child {
-        margin-bottom: 0;
-    }
+  .user-row:last-child {
+    margin-bottom: 0;
+  }
 
-    .dropdown-user {
-        margin: 13px 0;
-        padding: 5px;
-        height: 100%;
-    }
+  .dropdown-user {
+    margin: 13px 0;
+    padding: 5px;
+    height: 100%;
+  }
 
-    .dropdown-user:hover {
-        cursor: pointer;
-    }
+  .dropdown-user:hover {
+    cursor: pointer;
+  }
 
-    .table-user-information>tbody>tr {
-        border-top: 1px solid rgb(221, 221, 221);
-    }
+  .table-user-information>tbody>tr {
+    border-top: 1px solid rgb(221, 221, 221);
+  }
 
-    .table-user-information>tbody>tr:first-child {
-        border-top: 0;
-    }
+  .table-user-information>tbody>tr:first-child {
+    border-top: 0;
+  }
 
-    .table-user-information>tbody>tr>td {
-        border-top: 0;
-    }
+  .table-user-information>tbody>tr>td {
+    border-top: 0;
+  }
 
-    .toppad {
-        margin-top: 20px;
-    }
+  .toppad {
+    margin-top: 20px;
+  }
 
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -26,16 +26,6 @@ export default new Router({
         forAuth: true
       },
       children: [
-        // UserHome will be rendered inside User's <router-view>
-        // when /user/:id is matched
-        //{ path: '', component: UserHome },
-
-        // UserProfile will be rendered inside User's <router-view>
-        // when /user/:id/profile is matched
-        //{ path: 'profile', component: UserProfile },
-
-        // UserPosts will be rendered inside User's <router-view>
-        // when /user/:id/posts is matched
         {
           path: 'mySkills',
           component: mySkills
